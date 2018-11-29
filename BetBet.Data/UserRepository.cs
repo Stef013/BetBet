80473862c658ed42611c77a77e338a9f88d810d4
StefanV <stefan.vujinovic@student.fontys.nl>
2018-11-29 20:32:25 +0100

    Repositories gewijzigd

--- BetBet.Data/UserRepository.cs
+++ BetBet.Data/UserRepository.cs
@@ -3,51 +3,122 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 using BetBet.Model;
+using System.Diagnostics;
+using System.Data;
 
 namespace BetBet.Data
 {
     public class UserRepository : IRepository<User>
     {
-        BetBetDB database = new BetBetDB();
-        BetBetDBGoede dBGoede = new BetBetDBGoede();
+
+        public string Hostname = "studmysql01.fhict.local";
+        public string DBName = "dbi382222";
+        public string ID = "dbi382222";
+        public string Password = "vQzTCdskA2UBvfzp";
+
+        private MySqlConnection connection;
+
+        public UserRepository()
+        {
+            connection = new MySqlConnection($"SERVER={Hostname};DATABASE={DBName};UID={ID};PASSWORD={Password};SslMode=none");
+        }
+
+        private void OpenConnectionIfClosed()
+        {
+            if (connection.State != ConnectionState.Open)
+            {
+                connection.Open();
+            }
+        }
+
+        //BetBetDB database = new BetBetDB();
+        //BetBetDBGoede dBGoede = new BetBetDBGoede();
 
         public bool Create(User user)
         {
-            /* string checkUsername = $"SELECT Username FROM users WHERE Username = '{user.Username}'";
-             var namecheck = database.getString(checkUsername);
+            var namecheck = CheckUsername(user.Username);
+            bool result = false;
 
-             if(namecheck == null)
-             {
-                 string command = $"INSERT into users (Username, Password, Balance, IsAdmin) VALUES ('{user.Username}','{user.Password}','{0.00}','{0}')";
-                 database.executeCMD(command);
-                 return true;
-             }
-             else
-             {
-                 return false;
-             } */
-
-            List<User> users = dBGoede.GetAllUsers();
-            return true;
+            if (namecheck == null)
+            {
+                string command = $"INSERT into users (Username, Password, Balance, IsAdmin) VALUES ('{user.Username}','{user.Password}','{0.00}','{0}')";
+                using (MySqlCommand cmd = new MySqlCommand(command, connection))
+                {
+                    try
+                    {
+                        OpenConnectionIfClosed();
+                        cmd.ExecuteNonQuery();
+                        result = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(ex.Message);
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+                return result;
+            }
+            else
+            {
+                return false;
+            }            
         } 
 
         public int GetID(User user)
         {
             string command = $"SELECT UserID FROM users WHERE Username = '{user.Username}'";
-            int id = database.getID(command);
+            int id = 0;
 
+            using (MySqlCommand cmd = new MySqlCommand(command, connection))
+            {
+                try
+                {
+                    OpenConnectionIfClosed();
+                    id = (int)cmd.ExecuteScalar();
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
             return id;
         }
 
         public bool Delete(User user)
         {
-            if (user != null)
+            bool result = false;
+            int id = GetID(user);
+
+            if (id != 0)
             {
-                int id = GetID(user);
                 string command = $"DELETE * Where UserID =  '{id}'";
-                database.ExecuteCMD(command);
-                return true;
+                using (MySqlCommand cmd = new MySqlCommand(command, connection))
+                {
+                    try
+                    {
+                        OpenConnectionIfClosed();
+                        cmd.ExecuteNonQuery();
+                        result = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(ex.Message);
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+                return result;
             }
             else
             {
@@ -61,26 +132,61 @@ namespace BetBet.Data
             return true;
         }
 
-        public bool ComparePassword(string username, string password)
+        public string CheckUsername(string username)
         {
-            string getPW = $"SELECT Password FROM Users WHERE Username = '{username}'";
-            string pw = database.getString(getPW);
-            
-            if (pw != null)
+            string command = $"SELECT Username FROM users WHERE Username = '{username}'";
+            string result = null;
+            using (MySqlCommand cmd = new MySqlCommand(command, connection))
             {
-                if (string.Compare(password, pw) == 0)
+                try
                 {
-                    return true;
+                    OpenConnectionIfClosed();
+                    result = (string)cmd.ExecuteScalar();
                 }
-                else
+                catch (Exception ex)
                 {
-                    return false;
+                    Debug.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
                 }
             }
-            else
+            return result;
+        }
+
+        public bool ComparePassword(string username, string password)
+        {
+            bool result = false;
+            string command = $"SELECT Password FROM Users WHERE Username = '{username}'";
+
+            using (MySqlCommand cmd = new MySqlCommand(command, connection))
             {
-                return false;
-            }      
+                try
+                {
+                    OpenConnectionIfClosed();
+                    string passwordresult = (string)cmd.ExecuteScalar();
+
+                    if (string.Compare(password, passwordresult) == 0)
+                    {
+                        result = true;
+                    }
+                    else
+                    {
+                        result = false;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+
+            return result;
         }
     }
 }
